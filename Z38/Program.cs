81cab11e343895c10181fb8456c2c22403ad2e4b
Irina_Leskova <krip@mail.ru>
2023-06-25 15:36:26 +0500

    не сделала задания

---
+++ Z38/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+*/
+Console.Clear();
+Console.Write ("Введите элементы массива через пробел");
+string elements = Console.Readline()!;
+int baseArray = GetArrayFromString(elements);
+Console.WriteLine(String.Join(" ", ResultArray(baseArray)));
+Double[] GetArrayFromString(string stringArray)
+{string[] nums = stringArray.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+double [] res = new double[nums.Length];
+double minValua = nums.Length[0];
+double maxValua = nums.Length[0];
+double result;
+for(double i = 1; i <res.Length+1; i++)
+{
+    if (resArray[i]<minValua)
+   {
+    minValua=resArray[i];
+   } 
+   if(resArray[i]<maxValua)
+   {
+    maxValua=resArray[i];
+   }
+}
+Console.WriteLine("Разница максимального и минимального в массиве: {result}");
+}
---
+++ z34/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных 
+чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+int[] GetRanomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+  for (int i = 0; i < size; i++)
+    {
+result[i] = new Random().Next(minValue,maxValue+1);
+} 
+return result;
+}       
+    int[] arr = GetRanomArray(3,100,999); 
+    int = a;
+    int sum = 0;              
+             for (; i <= arr.Length; i++)
+        {
+            if (a%2 == 0)
+            {
+                sum = sum + 1;
+            }
+           
+           
+            Console.Write($"Четных чисел: {sum}");      
+    
+}
